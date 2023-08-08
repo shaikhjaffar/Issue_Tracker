a37c4666bc65aba8b439afc1ac3079584264d58e
[firstname <jaffar.shaikh@1clickcapital.com>
2023-08-08 15:18:49 +0530

    Issue_added

--- routes/route.js
+++ routes/route.js
@@ -3,17 +3,27 @@ const router = express.Router()
 const Issue = require('../models/Issue')
 const mongoose = require('mongoose')
 const Project = require('../models/project')
-
+let AllIssue = []
 
 router.get("/Issue",(req,res)=>{
-    Issue.find({})
-    .then((issue)=>{
+
+    Project.find({})
+   
+    .then((project)=>{
+
+     for (let i =0 ; i < project.length ;i++){
+        AllIssue.push(project[i].Issue)
+     }
+     console.log(AllIssue)
+ 
      res.render("showIssue",{
          tittle:"Issue",
-         issue:issue
+         project:project,
+         issue:AllIssue
      })
  
     }) 
+
      .catch((err)=>{
          res.json({
              message:err.message
@@ -55,6 +65,19 @@ router.get("/Issue",(req,res)=>{
      .then((project)=>{
         res.render("issue",{
             tittle:"Create Issue",
+            project :project
+        })
+     })
+     .catch((err)=>{
+        res.redirect("/")
+     })
+  })
+  router.get("/projectData/:id",(req,res)=>{
+    let id = req.params.id
+    Project.findById(id)
+     .then((project)=>{
+        res.render("project",{
+            tittle:"Project",
             project :project
         })
      })
